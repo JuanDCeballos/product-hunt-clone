3da8a5ce268384852fa63584d50910e4bca569f0
Juan Ceballos <juandavi789@hotmail.com>
2024-04-08 23:07:30 -0500

    Update tailwindcss configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <p>This is a test</p>
     </>
   );
