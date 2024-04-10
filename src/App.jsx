70f2b2a8d8a2a1bc1c38b14edc75edd023dcf72e
Juan Ceballos <juandavi789@hotmail.com>
2024-04-09 23:57:58 -0500

    Import modal component and create basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+import Modal from './Product/Modal';
+
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const closeModal = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <p>This is a test</p>
+      <button onClick={openModal}>Open</button>
+      <Modal isOpen={isOpen} closeModal={closeModal} />
     </>
   );
 };
