873d532cf2823abf01d60d48ee414237f014ab7f
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-04-11 23:01:09 -0500

    - ADD: The component RelatedTopics was created according to the original desing

--- src/Home/Components/HomePageComponent.jsx
+++ src/Home/Components/HomePageComponent.jsx
@@ -1,12 +1,13 @@
 import { ProductListComponent } from "../../Product/Components/ProductListComponent";
 import { TopLaunches } from "../../Home/Components/TopLaunches.jsx";
+import { RelatedTopicsComponent } from "./RelatedTopicsComponent.jsx";
 
 export const HomePageComponent = () => {
     return (
         <>
-            <div className="flex flex-row">
+            <div className="flex flex-row mt-12">
                 <div
-                    className="w-3/5 border-r border-inherit px-12 flex flex-col space-y-8 ml-36 mt-6 mr-10"
+                    className="w-3/5 border-r border-inherit px-12 flex flex-col space-y-8 ml-36 mr-10"
                 >
                     <div className="flex flex-col space-y-12 ">
                         <ProductListComponent Title="Top Products Launching Today" />
@@ -16,8 +17,9 @@ export const HomePageComponent = () => {
                     </div>
                 </div>
 
-                <div>
+                <div className="w-1/5">
                     <TopLaunches />
+                    <RelatedTopicsComponent />
                 </div>
             </div>
         </>
