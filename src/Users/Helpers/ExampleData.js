fa293e19de73a2c26648087b65c9ba65544bf89a
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-04-16 20:04:29 -0500

    - ADD: An User ExampleData was set
    - CHG: The Component UserProfile was modified to use the UseContext a desestructure the CurrentUser in the context
    
    - CHG: The component LogInComponent when sets the current logged In User use the User ExampleData

---
+++ src/Users/Helpers/ExampleData.js
@@ -0,0 +1,16 @@
+export const UserExampleData = {
+    "UserName": "Jh melísimo",
+    "UserID": 777,
+    "Picture": "https://www.kienyke.com/sites/default/files/styles/interna_contenido_s/public/2023-04/JH%20de%20la%20Cruz%20historia_0001_9.jpg?itok=VZnny0nN",
+    "CommunityMember": "Cristianos del mundo",
+    "Streak": 19,
+    "Followers": 10,
+    "Following": 99,
+    "Profile": "Soy un misionero de Dios que ayuda a todo el mundo.",
+    "Work": "Taxi Driver",
+    "Email": "JH@productHunt.com",
+    "PassWord": 12345,
+    "CreatedDateTime": "12-12-2009",
+    "ModifiedDateTime": "1-1-2024",
+    "BIO": "JH es un ingeniero de software dedicado y cristiano devoto, nacido en un pequeño pueblo de Portugal. Desde joven, mostró una notable habilidad para la programación y una profunda fe en sus creencias religiosas. Después de graduarse en ingeniería informática de la Universidad de Lisboa, se mudó a Silicon Valley para trabajar en proyectos de software que promueven la inclusión y el bienestar social. JH combina su pasión por la tecnología con su compromiso cristiano, participando activamente en iniciativas comunitarias y proyectos tecnológicos en su iglesia local. A través de su trabajo, busca mejorar la vida de las personas y servir a su comunidad."
+}
