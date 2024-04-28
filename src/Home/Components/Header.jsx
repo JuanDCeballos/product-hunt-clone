64f5af9be82559b4bb79ff4247689c44da6b518b
Juan Ceballos <juandavi789@hotmail.com>
2024-04-28 16:05:48 -0500

    Conditionally render Log in or user profile

--- src/Home/Components/Header.jsx
+++ src/Home/Components/Header.jsx
@@ -101,28 +101,45 @@ export const Header = () => {
             </li>
           </ul>
           <div className="flex items-center space-x-4">
-            <Link to="SumbitProduct">
-              <button className="text-white transition ease-in-out delay-150 bg-orange-500 hover:-translate-y-1 hover:scale-110  duration-300 w-32 h-8 rounded-lg">
-                Submit
-              </button>
-            </Link>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
-              />
-            </svg>
-            <Link to={TargetPath}>
-              <img src={ImageURL} className="w-10 h-10 rounded-full" />
-            </Link>
+            {!Logged ? (
+              <>
+                <Link to="UnderConstruction">
+                  <p className="text-white transition ease-in-out delay-150 hover:text-gray-300 w-32 h-8 rounded-lg">
+                    How to post?
+                  </p>
+                </Link>
+                <Link to={TargetPath}>
+                  <button className="text-white transition ease-in-out delay-150 bg-orange-500 hover:-translate-y-1 hover:scale-110  duration-300 w-32 h-8 rounded-lg">
+                    Log In
+                  </button>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link to="SumbitProduct">
+                  <button className="text-white transition ease-in-out delay-150 bg-orange-500 hover:-translate-y-1 hover:scale-110  duration-300 w-32 h-8 rounded-lg">
+                    Submit
+                  </button>
+                </Link>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
+                  />
+                </svg>
+                <Link to={TargetPath}>
+                  <img src={ImageURL} className="w-10 h-10 rounded-full" />
+                </Link>
+              </>
+            )}
           </div>
         </nav>
       </header>
