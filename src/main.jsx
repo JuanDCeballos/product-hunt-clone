bd12c7e7546c48419d16f632700b6c7d93925bfa
Juan Ceballos <juandavi789@hotmail.com>
2024-04-28 19:52:26 -0500

    Move BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { PageNotFound } from './ShareUI/Components/PageNotFound.jsx';
+import BrowserRouter from './Routing/BrowserRouter/BrowserRouter.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <PageNotFound />,
-
-    children: [
-      { path: 'Product' },
-      { path: 'launches' },
-      { path: 'SumbitProduct' },
-      { path: 'UserProfile' },
-      { path: 'LogIn' },
-      { path: 'UnderConstruction' },
-    ],
-  },
-]);
+const router = createBrowserRouter(BrowserRouter);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
