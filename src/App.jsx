16e531b5539de478c86b96c9600e06afa0c5b062
Juan Ceballos <juandavi789@hotmail.com>
2024-04-28 19:48:40 -0500

    Private routing was set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { HomePageComponent } from './Home/Components/HomePageComponent';
 import { Header } from './Home/Components/Header';
-import { Route, Routes } from 'react-router-dom';
-import { ProductForm } from './Product/Components/ProductForm';
-import { UserProfile } from './Users/Components/UserProfile.jsx';
 import { UserProvider } from './Users/Contexts/Providers/UserProvider.jsx';
-import { LogInComponent } from './Login/Components/LogInComponent.jsx';
 import { ProductProvider } from './Product/Contexts/Provider/ProductProvider.jsx';
-import UnderContrusction from './ShareUI/Components/UnderConstruction.jsx';
+import AppRouting from './Routing/AppRouter.jsx';
 
 const App = () => {
   return (
@@ -14,13 +9,7 @@ const App = () => {
       <UserProvider>
         <ProductProvider>
           <Header />
-          <Routes>
-            <Route path="*" element={<HomePageComponent />} />
-            <Route path="SumbitProduct" element={<ProductForm />} />
-            <Route path="UserProfile" element={<UserProfile />} />
-            <Route path="LogIn" element={<LogInComponent />} />
-            <Route path="UnderConstruction" element={<UnderContrusction />} />
-          </Routes>
+          <AppRouting />
         </ProductProvider>
       </UserProvider>
     </>
