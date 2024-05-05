3839e7ecd7f8bbb73d375c638376521c2e5b8bc1
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-05-05 15:25:42 -0500

    - ADD: Firebase file was added to project and its configuration

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import * as dotenv from 'dotenv';
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  setDoc,
+  updateDoc,
+} from 'firebase/firestore';
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
