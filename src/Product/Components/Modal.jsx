b38d52074689d9f0f44ddaab2b1f73ecb9af5180
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-05-09 00:42:33 -0500

    - CHG: Modal has been adapt to create comments in a product

--- src/Product/Components/Modal.jsx
+++ src/Product/Components/Modal.jsx
@@ -12,12 +12,18 @@ import { useContext, useRef, useState } from 'react';
 import { ProductContext } from '../Contexts/Context/ProductContext';
 import { useEffect } from 'react';
 import { LogInContext } from '../../Login/Context/LogInContext';
+import { toast } from 'sonner';
+import { addCommentInProduct } from '../../Firebase/Functions';
 
 Modal.setAppElement(document.getElementById('root'));
 
 const ProductView = ({ isOpen, closeModal }) => {
   const textAreaRef = useRef();
   const [val, setVal] = useState('');
+  const [rating, setRating] = useState(0);
+
+  const { user } = useContext(LogInContext);
+  const { product } = useContext(ProductContext);
 
   const onInputChange = (e) => {
     setVal(e.target.value);
@@ -31,9 +37,6 @@ const ProductView = ({ isOpen, closeModal }) => {
     }
   }, [val]);
 
-  const { product } = useContext(ProductContext);
-  const { User } = useContext(LogInContext);
-
   const {
     picture,
     productName,
@@ -42,6 +45,36 @@ const ProductView = ({ isOpen, closeModal }) => {
     longDescription,
     MediaLink,
   } = product;
+
+  const onChangeStarsRating = (newRating) => {
+    setRating(newRating);
+  };
+
+  const onComment = () => {
+    if (!val) {
+      toast.warning('You need to write something first!');
+      return;
+    }
+
+    const comment = {
+      Message: val,
+      UserName: user.displayName,
+      UserUID: user.uid,
+      UserPhotoURL: user.photoURL,
+      UserDescription: user.profileDesc,
+      Rating: rating,
+    };
+
+    toast.promise(addCommentInProduct(product.id, comment), {
+      loading: 'Saving comment...',
+      error: 'An error ocurred while trying to comment.',
+      success: () => {
+        setVal('');
+        setRating(0);
+        return 'Comment sended!';
+      },
+    });
+  };
 
   return (
     <>
@@ -106,7 +139,7 @@ const ProductView = ({ isOpen, closeModal }) => {
               >
                 <div>
                   <iframe
-                    src="https://www.youtube.com/embed/C4Q2ezioTUM?si=5ICB4qL4rbhNggBk"
+                    src={MediaLink}
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerPolicy="strict-origin-when-cross-origin"
@@ -139,15 +172,17 @@ const ProductView = ({ isOpen, closeModal }) => {
                     What do you think of {productName}?
                   </h2>
                   <ReactStars
+                    value={rating}
                     count={5}
                     size={34}
                     activeColor="#ffd700"
                     isHalf={true}
+                    onChange={onChangeStarsRating}
                   ></ReactStars>
                 </div>
                 <div className="flex justify-between w-full">
                   <img
-                    src="https://www.kienyke.com/sites/default/files/styles/interna_contenido_s/public/2023-04/JH%20de%20la%20Cruz%20historia_0001_9.jpg?itok=VZnny0nN"
+                    src={user.photoURL}
                     className="w-10 h-10 rounded-full m-4"
                   />
                   <textarea
@@ -161,7 +196,10 @@ const ProductView = ({ isOpen, closeModal }) => {
                   ></textarea>
                 </div>
                 <div className="flex w-full justify-end items-end m-2.5">
-                  <button className="w-36 bg-red-500 rounded-md flex items-center text-center justify-center text-white p-1.5 hover:bg-red-600">
+                  <button
+                    onClick={onComment}
+                    className="w-36 bg-red-500 rounded-md flex items-center text-center justify-center text-white p-1.5 hover:bg-red-600"
+                  >
                     Comment
                   </button>
                 </div>
