ae64c0cef989ead9328b5c0875fd004b35cb84b5
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-05-09 23:26:48 -0500

    - CHG: ProductRecuderTypes has been added to project

---
+++ src/Product/Contexts/Types/ProductReducerTypes.js
@@ -0,0 +1,7 @@
+export const ProductReducerTypes = {
+  setProductToShowInModal: '[ProductReducerTypes] SetProductToShowInModal',
+  setProductEdit: '[ProductReducerTypes] SetProductToShowInModal',
+  deleteProductToShowInModal:
+    '[ProductReducerTypes] DeleteProductToShowInModal',
+  deleteProductToEdit: '[ProductReducerTypes] DeleteProductToEdit',
+};
