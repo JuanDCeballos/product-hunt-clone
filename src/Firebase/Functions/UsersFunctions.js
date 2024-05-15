e786c6ad0a726f14f3023ada1ba55fbde61b99a5
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-05-14 23:10:11 -0500

    - CHG: Minor fix in function GetUsersList

--- src/Firebase/Functions/UsersFunctions.js
+++ src/Firebase/Functions/UsersFunctions.js
@@ -56,7 +56,6 @@ export async function GetUsersList(currentUserUID, userProvider) {
     const gitHubUsersResult = await getDocs(gitHubCollectionRefernce);
     const googleUsersResult = await getDocs(googleCollectionReference);
     let users = [];
-    console.log(followedUsers.includes('G9iEyIUyA7fOZq3tyDlXNcRKPrl2'));
     gitHubUsersResult.forEach((user) => {
       if (user.id != currentUserUID) {
         users.push({
