cb41a05fab624260b03b6188525cbafd721df6c2
Juan Ceballos <juandavi789@hotmail.com>
2024-05-15 22:47:27 -0500

    Add constants

---
+++ src/Home/helpers/const.js
@@ -0,0 +1,84 @@
+export const categoryOptions = [
+  {
+    id: 1,
+    name: 'Business',
+  },
+  {
+    id: 2,
+    name: 'Analytics',
+  },
+  {
+    id: 3,
+    name: 'Design',
+  },
+  {
+    id: 4,
+    name: 'Database',
+  },
+  {
+    id: 5,
+    name: 'Communication',
+  },
+  {
+    id: 6,
+    name: 'Storage',
+  },
+  {
+    id: 7,
+    name: 'Development',
+  },
+  {
+    id: 8,
+    name: 'Management',
+  },
+  {
+    id: 9,
+    name: 'Security',
+  },
+  {
+    id: 10,
+    name: 'Productivity',
+  },
+];
+
+export const platformOptions = [
+  {
+    id: 1,
+    name: 'Android',
+  },
+  {
+    id: 2,
+    name: 'Web',
+  },
+  {
+    id: 3,
+    name: 'Windows',
+  },
+  {
+    id: 4,
+    name: 'iOS',
+  },
+  {
+    id: 5,
+    name: 'Linux',
+  },
+  {
+    id: 6,
+    name: 'Mac',
+  },
+];
+
+export const productTypeOptions = [
+  {
+    id: 1,
+    name: 'SaaS',
+  },
+  {
+    id: 2,
+    name: 'PaaS',
+  },
+  {
+    id: 3,
+    name: 'IaaS',
+  },
+];
