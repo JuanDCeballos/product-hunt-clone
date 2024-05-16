3851af90bd5efdc8e29133d5c534c0ebd4633a9c
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-05-15 21:25:12 -0500

    - CHG: Related topics component has been modified to show a border on his top

--- src/Home/Components/RelatedTopicsComponent.jsx
+++ src/Home/Components/RelatedTopicsComponent.jsx
@@ -1,8 +1,8 @@
 export const RelatedTopicsComponent = () => {
   return (
     <>
-      <div className="flex h-28 space-x-4 mt-6">
-        <ul className="list-dot">
+      <div className="flex h-28 space-x-4 mt-6 border-t border-inherit">
+        <ul className="list-dot mt-4">
           <li className="text-sm"> © 2024 El equipo de Thomas </li>
         </ul>
       </div>
